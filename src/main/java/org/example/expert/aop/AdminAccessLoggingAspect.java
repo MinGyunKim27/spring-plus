9c4439de78bfdff2caa376c08fe2981caab1a14c
김민균 <kmg02845@gmail.com>
2025-07-03 15:20:44 +0900

    fix:LV11

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.example.expert.domain.common.dto.CustomUserDetails;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -21,11 +22,20 @@ public class AdminAccessLoggingAspect {
 
     @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
-        String userId = String.valueOf(request.getAttribute("userId"));
+
+        Long userIdFound = 0L;
+
+        for (Object arg : joinPoint.getArgs()) {
+            if (arg instanceof Long userId) {
+                userIdFound = userId;
+                log.info("User ID: {}", userId);
+            }
+        }
+
         String requestUrl = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
 
         log.info("Admin Access Log - User ID: {}, Request Time: {}, Request URL: {}, Method: {}",
-                userId, requestTime, requestUrl, joinPoint.getSignature().getName());
+                String.valueOf(userIdFound), requestTime, requestUrl, joinPoint.getSignature().getName());
     }
 }
